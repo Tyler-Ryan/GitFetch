4449a35c95feaabaa3b5488fa1b2ca41c72fee9a
Tyler Ryan <46903434+Tyler-Ryan@users.noreply.github.com>
2019-06-03 10:08:33 -0600

    Create app.js
    Create app.js to fetch data from github api

---
+++ app.js
@@ -0,0 +1,28 @@
+const inputValue = document.querySelector("#search");
+const searchButton = document.querySelector(".searchButton");
+const nameContainer = document.querySelector(".main__profile-name");
+const unContainer = document.querySelector(".main__profile-username");
+const reposContainer = document.querySelector(".main__profile-repos");
+const urlContainer = document.querySelector(".main__profile-url");
+
+const client_id = "Iv1.d751536202411dc1"
+const client_secret = "03867507e25450b5f90394e5e3ce6fe0881a3fa4"
+
+const fetchUsers = async (user) => {
+    const api_call = await fetch(`https://api.github.com/users/${user}?client_id=${client_id}&client_secret=${client_secret}`);
+    const data = await api_call.json();
+    return {data}
+}
+
+const showData = () => {
+    fetchUsers(inputValue.value).then((res) => {
+        nameContainer.innerHTML = `Name: <span class="main__profile-value">${res.data.name}</span>`;
+        unContainer.innerHTML = `Username: <span class="main__profile-value">${res.data.login}</span>`;
+        reposContainer.innerHTML = `Repos: <span class="main__profile-value">${res.data.public_repos}</span>`;
+        urlContainer.innerHTML = `URL: <span class="main__profile-value">${res.data.html_url}</span>`;
+    })
+}
+
+searchButton.addEventListener("click", () => {
+    showData();
+});
